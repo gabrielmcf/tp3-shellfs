c6d83a4cbde31f9e6c3144fe894bd7f8835c7722
gabriel <gabriel@localhost.localdomain>
2018-05-31 11:51:14 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <ext2fs/ext2fs.h>
+#include <com_err.h>
+
+int main(int argc, char *argv[]) 
+{ 
+    char *fsname = argv[1];
+    errcode_t err;
+    ext2_filsys fs;
+    add_error_table(&et_ext2_error_table);
+    
+    err = ext2fs_open(fsname, 0, 0, 0, unix_io_manager, &fs);
+
+    if (err) {
+        com_err(argv[0], err, "ao abrir sistema de arquivos %s", fsname);
+        exit(1);
+    }
+
+    // ext2fs_re
+}
