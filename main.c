50ff25bdc347dc3709c292d42a2270eac7eef6c9
gabriel <gabriel@localhost.localdomain>
2018-06-10 21:26:59 -0300

    Iniciado comando stat porém ainda com problemas

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #include "inc/directoryentry.h"
 
 struct os_superblock_t *superblock;
-struct os_blockgroup_descriptor_t *blockgroup;
+struct os_blockgroup_descriptor_t *group_descr;
 struct os_inode_t *inodes;
 
 void read_superblock(int fd)
@@ -27,29 +27,29 @@ void read_superblock(int fd)
 	assert(read(fd, (void *)superblock, sizeof(struct os_superblock_t)) == sizeof(struct os_superblock_t));
 }
 
-void read_blockgroup(int fd)
-{
-	blockgroup = malloc(sizeof(struct os_blockgroup_descriptor_t));
-	assert(blockgroup != NULL);
+// void read_blockgroup(int fd)
+// {
+// 	blockgroup = malloc(sizeof(struct os_blockgroup_descriptor_t));
+// 	assert(blockgroup != NULL);
        
-	assert(lseek(fd, (off_t)2048, SEEK_SET) == (off_t)2048);
-	assert(read(fd, (void *)blockgroup, sizeof(struct os_blockgroup_descriptor_t)) == sizeof(struct os_blockgroup_descriptor_t));
-}
+// 	assert(lseek(fd, (off_t)2048, SEEK_SET) == (off_t)2048);
+// 	assert(read(fd, (void *)blockgroup, sizeof(struct os_blockgroup_descriptor_t)) == sizeof(struct os_blockgroup_descriptor_t));
+// }
 
 
-void read_inodeTable(int fd)
-{
+// void read_inodeTable(int fd)
+// {
 
-	// preparing to cache inode table in inodes
-	inodes = (struct os_inode_t*)malloc(superblock->s_inodes_count*superblock->s_inode_size);
-	assert(inodes != NULL);
+// 	// preparing to cache inode table in inodes
+// 	inodes = (struct os_inode_t*)malloc(superblock->s_inodes_count*superblock->s_inode_size);
+// 	assert(inodes != NULL);
 
-	// seek to start of inode_table
-	assert(lseek(fd, (off_t)(blockgroup->bg_inode_table*1024), SEEK_SET) == (off_t)(blockgroup->bg_inode_table*1024));
+// 	// seek to start of inode_table
+// 	assert(lseek(fd, (off_t)(blockgroup->bg_inode_table*1024), SEEK_SET) == (off_t)(blockgroup->bg_inode_table*1024));
 
-	assert(read(fd, (void *)inodes, 0x40000) == 0x40000);
+// 	assert(read(fd, (void *)inodes, 0x40000) == 0x40000);
 
-}
+// }
 
 void printInodeType(int inode_type)
 {
@@ -102,6 +102,42 @@ void printInodePerm(int fd, int inode_num)
 	//lseek(fd, curr_pos, SEEK_SET);
 }
 
+int findInodeByName(int fd, int base_inode_num, char* filename, int filetype)
+{
+	char* name;
+	int curr_inode_num;
+	int curr_inode_type;
+
+	// debug("data block addr\t= 0x%x\n", inodes[base_inode_num-1].i_block[0]);
+
+	struct os_direntry_t* dirEntry = malloc(sizeof(struct os_direntry_t));
+	assert (dirEntry != NULL);
+	assert(lseek(fd, (off_t)(inodes[base_inode_num-1].i_block[0]*1024), SEEK_SET) == (off_t)(inodes[base_inode_num-1].i_block[0]*1024));
+	assert(read(fd, (void *)dirEntry, sizeof(struct os_direntry_t)) == sizeof(struct os_direntry_t));
+
+	 while (dirEntry->inode) {
+
+		name = (char*)malloc(dirEntry->name_len+1);
+		memcpy(name, dirEntry->file_name, dirEntry->name_len);
+		name[dirEntry->name_len+1] = '\0';
+
+		curr_inode_num = dirEntry->inode;
+		curr_inode_type = dirEntry->file_type;
+
+		if (filetype == curr_inode_type) {
+			if(!strcmp(name, filename)) {
+				return(curr_inode_num);
+			}
+		}
+
+		lseek(fd, (dirEntry->rec_len - sizeof(struct os_direntry_t)), SEEK_CUR);
+		assert(read(fd, (void *)dirEntry, sizeof(struct os_direntry_t)) == sizeof(struct os_direntry_t));
+
+	} 
+
+	return(-1);	
+}
+
 
 void ls(int fd, int base_inode_num)
 {
@@ -145,6 +181,89 @@ void ls(int fd, int base_inode_num)
 	return;
 }
 
+int cd(int fd, int base_inode_num)
+{
+	char dirname[255];
+	int ret;
+
+	//printf("Enter directory name:");
+	scanf("%s", dirname);
+
+	ret = findInodeByName(fd, base_inode_num, dirname, EXT2_FT_DIR);
+	// debug("findInodeByName=%d\n", ret);
+
+	if(ret==-1) {
+		printf("Directory %s does not exist\n", dirname);
+		return(base_inode_num);
+	} else {
+		printf("Now in directory %s\n", dirname);
+		return(ret);
+	}
+
+}
+
+void my_stat(int fd, int base_inode_num)
+{
+	char dirname[255];
+	char* name;
+	int ret;
+	struct os_inode_t inode;
+
+	scanf("%s", dirname);
+	// printf("li: %s\n",dirname);
+
+	ret = findInodeByName(fd, base_inode_num, dirname, EXT2_FT_DIR);
+
+	
+
+	if(ret==-1) {
+		printf("Arquivo %s não existe!\n", dirname);
+		return;
+	} else {
+		inode = inodes[ret-1];
+		printf("File: \"%s\"\n", dirname);
+		printf("Size: %d\t Blocks: %d\t", inode.i_size, inode.i_blocks);
+		printf("block size \t\t= %d bytes\n", 1<<(10 + superblock->s_log_block_size));
+		printf("inode count \t\t= %d\n", superblock->s_inodes_count);
+		printf("inode size \t\t= %d\n", superblock->s_inode_size);
+		printf("inode table address \t= %d\n", group_descr->bg_inode_table);
+		printf("inode table size \t= %dKB\n", (superblock->s_inodes_count*superblock->s_inode_size)>>10);
+		return;
+	}
+
+}
+
+int shellFs(int fd )
+{
+	char cmd[4];
+	static int pwd_inode = 2;
+
+	printf("dcc-shell-fs$ ");
+	scanf("%s", cmd);
+
+	// debug("cmd=%s\n", cmd);
+
+	if(!strcmp(cmd, "q")) {
+		return(-1);
+
+	} else if(!strcmp(cmd, "ls")) {
+		ls(fd, pwd_inode);
+
+	} else if(!strcmp(cmd, "cd")) {
+		pwd_inode = cd(fd, pwd_inode);
+
+	} 
+	else if(!strcmp(cmd, "stat")) {
+		my_stat(fd, pwd_inode);
+	} 
+	else {
+		printf("Unknown command: %s\n", cmd);
+		return(-EINVAL);
+	}
+
+	return(0);
+}
+
 int main(int argc, char **argv)
 {
 	// open up the disk file
@@ -160,29 +279,65 @@ int main(int argc, char **argv)
 	}
 
 	// reading superblock
-	read_superblock(fd);
-	printf("block size \t\t= %d bytes\n", 1<<(10 + superblock->s_log_block_size));
-	printf("inode count \t\t= 0x%x\n", superblock->s_inodes_count);
-	printf("inode size \t\t= 0x%x\n", superblock->s_inode_size);
+	// read_superblock(fd);
 
-	// reading blockgroup
-	read_blockgroup(fd);
-	printf("inode table address \t= 0x%x\n", blockgroup->bg_inode_table);
-	printf("inode table size \t= %dKB\n", (superblock->s_inodes_count*superblock->s_inode_size)>>10);
+	superblock = malloc(sizeof(struct os_superblock_t));
+	assert(superblock != NULL);
+       
+	assert(lseek(fd, (off_t)1024, SEEK_SET) == (off_t)1024);
+	assert(read(fd, (void *)superblock, sizeof(struct os_superblock_t)) == sizeof(struct os_superblock_t));
+
+	/* calculate number of block groups on the disk */
+	unsigned int group_count;
+	group_count = 1 + (superblock->s_blocks_count - 1) / superblock->s_blocks_per_group;
+
+	/* calculate size of the group descriptor list in bytes */
+	unsigned int descr_list_size;
+	descr_list_size = group_count * sizeof(struct os_blockgroup_descriptor_t);
+
+
+	
+
+
+	// struct os_blockgroup_descriptor_t group_descr;
+	/* position head above the group descriptor block */
+	/* sd --> storage device, no nosso caso um arquivo */
+	group_descr = malloc(sizeof(struct os_blockgroup_descriptor_t));
+	lseek(fd, (off_t)2048, SEEK_SET);
+	read(fd, (void *)group_descr, sizeof(struct os_blockgroup_descriptor_t));
+
+
+	// assert(blockgroup != NULL);
+	
+	// assert(lseek(fd, (off_t)2048, SEEK_SET) == (off_t)2048);
+	// assert(read(fd, (void *)blockgroup, sizeof(struct os_blockgroup_descriptor_t)) == sizeof(struct os_blockgroup_descriptor_t));
+
+	// // reading blockgroup
+	// read_blockgroup(fd);
+	
+
+	// preparing to cache inode table in inodes
+	inodes = (struct os_inode_t*)malloc(superblock->s_inodes_count*superblock->s_inode_size);
+	assert(inodes != NULL);
+
+	// seek to start of inode_table
+	assert(lseek(fd, (off_t)(group_descr->bg_inode_table*1024), SEEK_SET) == (off_t)(group_descr->bg_inode_table*1024));
+
+	assert(read(fd, (void *)inodes, 0x40000) == 0x40000);
 
 	// reading inode table
-	read_inodeTable(fd);
+	// read_inodeTable(fd);
 
-	// while(1) {
-	// 	// extShell waits for one cmd and executes it.
-	// 	// returns 0 on successfull execution of command,
-	// 	// returns -EINVAL on unknown command
-	// 	// returns -1 if cmd="q" i.e. quit.
-	// 	if ( extShell(fd)==-1 )
-	// 		break;
-	// }
+	while(1) {
+		// extShell waits for one cmd and executes it.
+		// returns 0 on successfull execution of command,
+		// returns -EINVAL on unknown command
+		// returns -1 if cmd="q" i.e. quit.
+		if ( shellFs(fd)==-1 )
+			break;
+	}
 
-    ls(fd, 2);
+    // ls(fd, 2);
 
 	printf("\n\nQuitting ext-shell.\n\n");
 	return(0);
